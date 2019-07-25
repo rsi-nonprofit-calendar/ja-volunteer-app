50b066bc3c4fb2bcb1b3b74e4396e717701957e3
Anton <loa.anton@yahoo.com>
2019-07-25 15:39:39 -0600

    deleted comments

--- src/app/services/create-event.service.ts
+++ src/app/services/create-event.service.ts
@@ -1,14 +1,2 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-
-// @Injectable({
-//   providedIn: "root"
-// })
-// // export class CreateEventService {
-// //   _url = "http://jsonplaceholder.typicode.com/posts";
-// //   constructor(private _http: HttpClient) {}
-
-// //   enrollcreate() {
-
-// //   }
-// // }
