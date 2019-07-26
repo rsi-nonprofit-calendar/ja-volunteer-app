8d6ae37ddabf2e0720c737a48d90c07cc55f61f8
William Isengard <william.isengard@gmail.com>
2019-07-26 13:33:41 -0600

    Created Sign In modal. Updated Jumbotron to include text card for details. Updated categories styling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
+import { SignInModal } from "./sign-in/sign-in.component";
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { CategoriesComponent } from "./categories/categories.component";
 import { NonprofitComponent } from "./nonprofit/nonprofit.component";
 import { ProfileComponent } from "./profile/profile.component";
 import { SplashComponent } from "./splash/splash.component";
-import { CalendarComponent } from './calendar/calendar.component';
-import { AboutUsComponent } from './about-us/about-us.component';
+import { CalendarComponent } from "./calendar/calendar.component";
+import { AboutUsComponent } from "./about-us/about-us.component";
 
 @NgModule({
   declarations: [
@@ -21,9 +22,16 @@ import { AboutUsComponent } from './about-us/about-us.component';
     ProfileComponent,
     SplashComponent,
     CalendarComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    SignInModal
   ],
-  imports: [NgbModule, BrowserModule, AppRoutingModule],
+  imports: [
+    NgbModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
